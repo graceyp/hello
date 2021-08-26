3d442298c5fb15ea7187ad2bbe8f6705ca229a41
Gracey <gperez8@huskers.unl.edu>
2021-08-26 17:45:58 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Gracey Perez and Renzo Sarreal
+ * Date: 2021/8/26
+ *
+ * A program that prints my name and my partners
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Gracey Perez and Rernzo Sarreal");
+
+    print 0;
+}
